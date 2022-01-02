08c4620f2e1bb5d8dea65cd9e06ba30da2002b68
yozakura <matchnokaori@gmail.com>
2022-01-03 01:04:30 +0900

    first message

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  Hello VUe3 and TypeScript!!
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style lang="scss">
+#app {
+
+}
+</style>
