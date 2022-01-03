d5c5eed00d40bbe0fc54d2c39d97ca617fd629a4
yozakura <matchnokaori@gmail.com>
2022-01-03 15:58:42 +0900

    一時保存

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,50 @@
 <template>
-  <div>
-    <h1>Hello VUe3 and TypeScript!!</h1>
-    <p>ちょっとだけsassもはいってます!</p>
+  <div class="container">
+    <h2>{{ name }}</h2>
+    <button @click="changeName('眠くなってきちゃうね')">上の文字を変更できるよ</button>
+    <p>{{ age }}歳だよ!</p>
+    <button @click="changeAge('34')">本当の年齢を見てみよう</button>
+    <p>{{ message }}</p>
+    <p>この文字が表示されないな</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      name: "Vue3 and Typescript",
+      age: 25 as string | number,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+      return age;
+    }
+  },
+  setup () {
+    const state = reactive<{ message:string }>({
+      message: "hello",
+    });
+    return {
+      state,
+    };
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-
+.container {
+  padding: 1rem; 
 }
 </style>
